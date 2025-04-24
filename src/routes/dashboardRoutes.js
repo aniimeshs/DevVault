2b42ad8dc76805b82dea954dfb7cc8068d6cad5c
aniimeshs <animeshpr152002@gmail.com>
2025-04-24 16:10:28 +0530

    Fixing path issues in routes

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getDashboardStats } = require("../src/controllers/dashboardController");
+const { getDashboardStats } = require("..controllers/dashboardController");
 const auth = require("../middleware/auth");
 
 router.get("/", auth, getDashboardStats);
